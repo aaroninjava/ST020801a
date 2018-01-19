29a1da828d2456e2473dd680fd34b67189b76340
aarontsai <aaroninpcschool241@gmail.com>
2018-01-19 08:19:56 +0800

    Initial commit

---
+++ app/src/main/java/com/example/aaroninjava/opt1/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.aaroninjava.opt1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+
+    /*
+        public void click2(View v)
+    {
+        EditText ed = findViewById(R.id.editText);
+        Intent it = new Intent(MainActivity.this, SecActivity.class);
+        it.putExtra("data", ed.getText().toString());
+        startActivity(it);
+
+     */
+
+
+}
